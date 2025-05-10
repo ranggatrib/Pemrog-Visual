050e1e1ec19384405f8ed97e7aef62675c7f4c3b
Ranzz986 <ranggatri86@gmail.com>
2025-05-11 05:04:50 +0700

    Rename folder CRUD-Produk menjadi Project

---
+++ Project/Project/CRUD-Produk/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace Project
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtNama = new System.Windows.Forms.TextBox();
+            this.txtDeskripsi = new System.Windows.Forms.TextBox();
+            this.txtHarga = new System.Windows.Forms.TextBox();
+            this.pbGambar = new System.Windows.Forms.PictureBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.dgvProduk = new System.Windows.Forms.DataGridView();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.labelId = new System.Windows.Forms.Label();
+            this.labelNama = new System.Windows.Forms.Label();
+            this.labelDeskripsi = new System.Windows.Forms.Label();
+            this.labelHarga = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGambar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProduk)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(127, 36);
+            this.txtId.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(168, 26);
+            this.txtId.TabIndex = 0;
+            // 
+            // txtNama
+            // 
+            this.txtNama.Location = new System.Drawing.Point(127, 80);
+            this.txtNama.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(168, 26);
+            this.txtNama.TabIndex = 1;
+            // 
+            // txtDeskripsi
+            // 
+            this.txtDeskripsi.Location = new System.Drawing.Point(127, 124);
+            this.txtDeskripsi.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtDeskripsi.Multiline = true;
+            this.txtDeskripsi.Name = "txtDeskripsi";
+            this.txtDeskripsi.Size = new System.Drawing.Size(168, 74);
+            this.txtDeskripsi.TabIndex = 2;
+            // 
+            // txtHarga
+            // 
+            this.txtHarga.Location = new System.Drawing.Point(127, 216);
+            this.txtHarga.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtHarga.Name = "txtHarga";
+            this.txtHarga.Size = new System.Drawing.Size(168, 26);
+            this.txtHarga.TabIndex = 3;
+            // 
+            // pbGambar
+            // 
+            this.pbGambar.Location = new System.Drawing.Point(346, 36);
+            this.pbGambar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pbGambar.Name = "pbGambar";
+            this.pbGambar.Size = new System.Drawing.Size(169, 150);
+            this.pbGambar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbGambar.TabIndex = 4;
+            this.pbGambar.TabStop = false;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(346, 208);
+            this.btnBrowse.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(169, 38);
+            this.btnBrowse.TabIndex = 5;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(59, 292);
+            this.btnCreate.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(90, 38);
+            this.btnCreate.TabIndex = 6;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(202, 291);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(90, 38);
+            this.btnUpdate.TabIndex = 7;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(348, 291);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(90, 38);
+            this.btnDelete.TabIndex = 8;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(501, 292);
+            this.btnLoad.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(90, 38);
+            this.btnLoad.TabIndex = 9;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.Click += new System.EventHandler(this.Form1_Load);
+            // 
+            // dgvProduk
+            // 
+            this.dgvProduk.ColumnHeadersHeight = 34;
+            this.dgvProduk.Location = new System.Drawing.Point(44, 352);
+            this.dgvProduk.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dgvProduk.Name = "dgvProduk";
+            this.dgvProduk.RowHeadersWidth = 62;
+            this.dgvProduk.Size = new System.Drawing.Size(562, 250);
+            this.dgvProduk.TabIndex = 10;
+            this.dgvProduk.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvProduk_CellClick);
+            // 
+            // labelId
+            // 
+            this.labelId.AutoSize = true;
+            this.labelId.Location = new System.Drawing.Point(44, 39);
+            this.labelId.Name = "labelId";
+            this.labelId.Size = new System.Drawing.Size(26, 20);
+            this.labelId.TabIndex = 11;
+            this.labelId.Text = "ID";
+            // 
+            // labelNama
+            // 
+            this.labelNama.AutoSize = true;
+            this.labelNama.Location = new System.Drawing.Point(44, 83);
+            this.labelNama.Name = "labelNama";
+            this.labelNama.Size = new System.Drawing.Size(51, 20);
+            this.labelNama.TabIndex = 12;
+            this.labelNama.Text = "Nama";
+            // 
+            // labelDeskripsi
+            // 
+            this.labelDeskripsi.AutoSize = true;
+            this.labelDeskripsi.Location = new System.Drawing.Point(44, 127);
+            this.labelDeskripsi.Name = "labelDeskripsi";
+            this.labelDeskripsi.Size = new System.Drawing.Size(74, 20);
+            this.labelDeskripsi.TabIndex = 13;
+            this.labelDeskripsi.Text = "Deskripsi";
+            // 
+            // labelHarga
+            // 
+            this.labelHarga.AutoSize = true;
+            this.labelHarga.Location = new System.Drawing.Point(44, 219);
+            this.labelHarga.Name = "labelHarga";
+            this.labelHarga.Size = new System.Drawing.Size(53, 20);
+            this.labelHarga.TabIndex = 14;
+            this.labelHarga.Text = "Harga";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(654, 625);
+            this.Controls.Add(this.labelId);
+            this.Controls.Add(this.labelNama);
+            this.Controls.Add(this.labelDeskripsi);
+            this.Controls.Add(this.labelHarga);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.txtNama);
+            this.Controls.Add(this.txtDeskripsi);
+            this.Controls.Add(this.txtHarga);
+            this.Controls.Add(this.pbGambar);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.dgvProduk);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "CRUD Produk";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.pbGambar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProduk)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelId;
+        private System.Windows.Forms.Label labelNama;
+        private System.Windows.Forms.Label labelDeskripsi;
+        private System.Windows.Forms.Label labelHarga;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtNama;
+        private System.Windows.Forms.TextBox txtDeskripsi;
+        private System.Windows.Forms.TextBox txtHarga;
+        private System.Windows.Forms.PictureBox pbGambar;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.DataGridView dgvProduk;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
